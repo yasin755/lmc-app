f0edb4db3c56c0174f80f3ed2dfc7fc17213cbcc
yasin <myasin@xemplarinsights.com>
2025-02-05 15:18:03 +0530

    build files added

--- backend/server.js
+++ backend/server.js
@@ -35,15 +35,25 @@ app.use(
   })
 );
 
-app.use(express.static("../client/build"));
+/*app.use(express.static("../client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-});
+});*/
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-  logger.info(req.cookies["mobileOtp"]);
-});
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    logger.info("Node Env: " + process.env.NODE_ENV);
+    res.send("API is running..");
+  });
+}
 
 app.get("/api/chats", (req, res) => {
   res.send(chats);
